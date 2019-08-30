5c5783d9aa644cc795dd1e51d9f70546a3119e5c
harshrajdhote <dhoteharshraj@gmail.com>
2019-08-30 12:06:45 +0530

    update even_odd

--- Ap/Exercise 1/string_permutations.java
+++ Ap/Exercise 1/string_permutations.java
@@ -1,9 +1,38 @@
-import java.util.*;
-class string_permutations
-{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str= sc.nextline();
-        for(int i = 0; i<str.)
-    }
-}
+ 
+public class Permutation 
+{ 
+	public static void main(String[] args) 
+	{ 
+		String str = "ABC"; 
+		int n = str.length(); 
+		Permutation permutation = new Permutation(); 
+		permutation.permute(str, 0, n-1); 
+	} 
+
+	private void permute(String str, int l, int r) 
+	{ 
+		if (l == r) 
+			System.out.println(str); 
+		else
+		{ 
+			for (int i = l; i <= r; i++) 
+			{ 
+				str = swap(str,l,i); 
+				permute(str, l+1, r); 
+				str = swap(str,l,i); 
+			} 
+		} 
+	} 
+
+	
+	public String swap(String a, int i, int j) 
+	{ 
+		char temp; 
+		char[] charArray = a.toCharArray(); 
+		temp = charArray[i] ; 
+		charArray[i] = charArray[j]; 
+		charArray[j] = temp; 
+		return String.valueOf(charArray); 
+	} 
+
+} 
