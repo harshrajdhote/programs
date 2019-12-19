05bca007e9ad2d04bacbabbb7e91c7ad53ba1ac8
harshrajdhote <dhoteharshraj@gmail.com>
2019-12-19 20:01:32 +0530

    array char ptr v.1

--- practice/pointers/array_char_ptr.cpp
+++ practice/pointers/array_char_ptr.cpp
@@ -15,5 +15,14 @@ int main(){
     p=p+1;
     // a=a+1; gives error
     p = a+ 1;
+    char str[] = "abc"; // this makes the array here copy of temp memory is created and str poits to that
+    //char *pstr = "abc" ; // here ptr points to temp memory , this temp space may be read only may get error or not
+    int as[] = {1,23,3};
+    char b[] = "abc";
+    cout<<as<<endl<<b; // as gives address
+    char * c  = &b[0]; // char *c prints till it finds \0
+    char c1  = 'a';
+    char *pc = &c1;
+    cout<<endl<<c<<endl<<c1<<endl<<pc; // here output that time was ad??? becoz it prints till it dont find \0
 
 }
