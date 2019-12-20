44aea76490d00988d95fbe9a43864f892b464a41
harshrajdhote <dhoteharshraj@gmail.com>
2019-12-20 23:14:18 +0530

    addresstypecasting

---
+++ practice/pointers/reference.cpp
@@ -0,0 +1,4 @@
+//https://www.educba.com/c-plus-plus-reference-vs-pointer/ difference ptrs and reference
+// references doesn't have its own address but the pointers has 
+// null cant be assigned to references
+// reassignment cant be done
