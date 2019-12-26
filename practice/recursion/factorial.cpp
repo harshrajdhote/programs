3ac7967985fa4376a03631eaaecd8846f50fa62c
harshrajdhote <dhoteharshraj@gmail.com>
2019-12-26 19:04:10 +0530

    recursion

---
+++ practice/recursion/factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+	cout << n << endl;
+	if (n == 0) {
+		return 1;
+	}
+	int smallOutput = factorial(n - 1);
+	return n * smallOutput;
+}
+//need of recursion
+//
+int main() {
+	int n;
+	cin >> n;
+	int output = factorial(n);
+	cout << output << endl;
+}
