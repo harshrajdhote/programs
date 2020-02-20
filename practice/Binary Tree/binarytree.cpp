3f66273d631a69d22f44d487b3823c817296d706
harshrajdhote <dhoteharshraj@gmail.com>
2020-02-20 19:17:14 +0530

    binary tree

--- practice/Binary Tree/binarytree.cpp
+++ practice/Binary Tree/binarytree.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void printTree(BinaryTreeNode<int>* root){
     if(root == NULL)
     return;
-    cout<<root->data<<endl;
+    cout<<root->data;
     if(root->left)
     cout<<"L"<<root->left->data;
     if(root->right)
@@ -12,13 +12,28 @@ void printTree(BinaryTreeNode<int>* root){
     cout<<endl;
     printTree(root->left);
     printTree(root->right);
+    //delete root;
 }
-using namespace std;
+BinaryTreeNode<int>* takeInput(){
+    int rootData;
+    cout<<"Enter data"<<endl;
+    cin>>rootData;
+    if(rootData == -1)
+    return NULL;
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    BinaryTreeNode<int>* left = takeInput();
+    BinaryTreeNode<int>* right = takeInput();
+    root->left = left;
+    root->right = right;
+    return root;
+}
 int main(){
-    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(1);
-    BinaryTreeNode<int> *n1 = new BinaryTreeNode<int>(2);
-    BinaryTreeNode<int> *n2 = new BinaryTreeNode<int>(3);
-    root->left = n1;
-    root->right = n2;
+    // BinaryTreeNode<int> *root = new BinaryTreeNode<int>(1);
+    // BinaryTreeNode<int> *n1 = new BinaryTreeNode<int>(2);
+    // BinaryTreeNode<int> *n2 = new BinaryTreeNode<int>(3);
+    // root->left = n1;
+    // root->right = n2;
+    BinaryTreeNode<int>* root = takeInput();
     printTree(root);
+    delete root;
 }
