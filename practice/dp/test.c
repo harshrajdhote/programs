6935eb947d33da02f5908dd46b9b3764a492c78d
harshrajdhote <dhoteharshraj@gmail.com>
2020-04-15 22:36:15 +0530

    windows on

--- practice/dp/test.c
+++ practice/dp/test.c
@@ -1,8 +1,29 @@
 #include<stdio.h>
-
+#define max(name,type) \
+        type name(type x,type y){ \
+        return (x > y) ? x : y; \
+        }
+ max(max_float,float)   
+ #define test 
+ #ifdef test
+ printf();
+ #endif    
 void main(){
-    int a;
-    scanf("%s",&a);
-    printf("op = %d",a);
+    // int a;
+    // scanf("%s",&a);
+    // printf("op = %d",a);
     //getch();
+    //   printf("ha\rraj\bas");
+    //   printf("%10s\n","India") ; printf("%4s\n","India") ; printf("%.2s\n","India") ; printf("%5.2s\n","India") ;
+    //   int a = 10;
+    //   int b = 3;
+    //   printf("%f",(a/b));
+       int y =  1;
+    //   int x = - -y;
+    //   printf("%d",x); o/p = 1 
+    //a;
+      printf("%f",max_float(2.3,3));
+      
 }
+
+// printf("%10s\n","India") ; printf("%4s\n","India") ; printf(".2s\n","India") ; printf("%5.2s\n","India") ;
