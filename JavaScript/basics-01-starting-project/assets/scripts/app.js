79c99fcebab96e0afc1d909a9f51ac7ad0bb0d8d
harshrajdhote <dhoteharshraj@gmail.com>
2020-08-18 19:23:19 +0530

    Lecture 36 parsing inputs

--- JavaScript/basics-01-starting-project/assets/scripts/app.js
+++ JavaScript/basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,7 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
 function add(){
-    currentResult = currentResult + userInput.value;
+    currentResult = currentResult + parseFloat(userInput.value);
     outputResult(currentResult,'');
 }
 addBtn.addEventListener('click',add);
