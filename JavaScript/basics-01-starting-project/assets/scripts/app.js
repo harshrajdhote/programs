3ebfdd7b0f8b9e88799f7f128c09a17f531c96a7
harshrajdhote <dhoteharshraj@gmail.com>
2020-08-18 22:38:27 +0530

    simpler calculator my version

--- JavaScript/basics-01-starting-project/assets/scripts/app.js
+++ JavaScript/basics-01-starting-project/assets/scripts/app.js
@@ -1,7 +1,34 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 function add(){
-    currentResult = currentResult + parseFloat(userInput.value);
-    outputResult(currentResult,'');
+    const enteredNumber = parseInt(userInput.value);
+    const calcDesp = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult +enteredNumber;//shortcut + +
+    userInput.value = 0;
+    outputResult(currentResult,calcDesp);
+}
+function divide(){
+    const enteredNumber = parseInt(userInput.value);
+    const calcDesp = `${currentResult} / ${enteredNumber}`;
+    currentResult = currentResult / enteredNumber;
+    userInput.value = 0;
+    outputResult(currentResult,calcDesp);
+}
+function multiply(){
+    const enteredNumber = parseInt(userInput.value);
+    const calcDesp = `${currentResult} * ${enteredNumber}`;
+    currentResult = currentResult * enteredNumber;
+    userInput.value = 0;
+    outputResult(currentResult,calcDesp);
+}
+function subtract(){
+    const enteredNumber = parseInt(userInput.value);
+    const calcDesp = `${currentResult} - ${enteredNumber}`;
+    currentResult = currentResult - enteredNumber;
+    userInput.value = 0;
+    outputResult(currentResult,calcDesp);
 }
 addBtn.addEventListener('click',add);
+multiplyBtn.addEventListener('click',multiply);
+subtractBtn.addEventListener('click',subtract);
+divideBtn.addEventListener('click',divide);
