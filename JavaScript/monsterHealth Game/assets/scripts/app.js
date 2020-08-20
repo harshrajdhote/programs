194cd50fd8ef8ed6934f522923229492ea7ab546
harshrajdhote <dhoteharshraj@gmail.com>
2020-08-20 19:18:34 +0530

    monster health project - strongattack and attack functionality  v1 added

---
+++ JavaScript/monsterHealth Game/assets/scripts/app.js
@@ -0,0 +1,39 @@
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+adjustHealthBars(chosenMaxLife);
+
+function attackMonster(mode){
+    let maxDamage;
+    if(mode === 'ATTACK')
+    {
+        maxDamage = ATTACK_VALUE;
+    }
+    else{
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if(currentMonsterHealth <= 0 && currentPlayerHealth > 0)
+    alert('You won!');
+    else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0)
+    alert('You lost!');
+    else if(currentMonsterHealth <=0 && currentPlayerHealth<=0)
+    alert('draw');
+    
+}
+
+function attackHandler(){
+    attackMonster('ATTACK');
+}
+
+function strongAttackHandler(){
+    attackMonster('STRONG_ATTACK');
+}
+attackBtn.addEventListener('click',attackHandler);
+strongAttackBtn.addEventListener('click',strongAttackHandler);
