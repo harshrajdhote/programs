448dc53d1f1c09236c24e7fbe7ac18fd8dbc753c
harshrajdhote <dhoteharshraj@gmail.com>
2020-08-21 13:22:09 +0530

    monster health project - write log function added

--- JavaScript/monsterHealth Game/assets/scripts/app.js
+++ JavaScript/monsterHealth Game/assets/scripts/app.js
@@ -75,8 +75,60 @@ function attackMonster(mode){
     endRound();
 }
 
-function writeLog(event){
- if(1){}
+function writeLog(event,value,monsterHealth,playerHealth){
+ let logEntry;
+ if(event === LOG_EVENT_PLAYER_ATTACK){
+     logEntry = {
+         event : event,
+         value : value,
+         target: 'MONSTER',
+         finalMonsterHealth: monsterHealth,
+         finalPlayerHealth: playerHealth
+     };
+     
+ }
+ else if(event === LOG_EVENT_PLAYER_STRONG_ATTACK){
+    logEntry = {
+        event : event,
+        value : value,
+        target: 'MONSTER',
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth
+    };
+    
+ }else if(event == LOG_EVENT_MONSTER_ATTACK){
+    logEntry = {
+        event : event,
+        value : value,
+        target: 'PLAYER',
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth
+    };
+
+ }else if(event === LOG_EVENT_PLAYER_HEAL){
+    logEntry = {
+        event : event,
+        value : value,
+        target: 'PLAYER',
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth
+    };
+
+ }else if(event === LOG_EVENT_GAME_OVER){
+    logEntry = {
+        event : event,
+        value : value,
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth
+    };
+   
+ }else
+ {
+    logEntry = {
+        event: 'OOPS, SOMETHING WENT WRONG!'
+    };
+ }
+ battleLog.push(logEntry);
 }
 
 function attackHandler(){
