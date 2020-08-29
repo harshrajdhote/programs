8e78984c47a7b4074466db2aef63a19ecc1c493a
harshrajdhote <dhoteharshraj@gmail.com>
2020-08-29 15:27:49 +0530

    Movie Project Add Movie Rendering Done V.1

--- JavaScript/Movie Project/assets/scripts/app.js
+++ JavaScript/Movie Project/assets/scripts/app.js
@@ -4,6 +4,7 @@ const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text')
 const movies = [];
 const clearMovieInput = () => {
     for(const usrInput of userInputs)
@@ -39,11 +40,39 @@ const addMovieHandler = ()=>{
         image : imageUrlValue,
         rating : ratingValue
     };
+    movies.push(newMovie);
     console.log(newMovie);
     toggleMovieModalHandler();
     clearMovieInput();
+    renderNewMovieElement(newMovie.title,newMovie.image,newMovie.rating);
+    UpdateUI();
 
-}
+};
+const UpdateUI = () =>{
+        if(movies.length === 0){
+            entryTextSection.style.display = 'block';
+        }
+        else
+        {
+            entryTextSection.style.display = 'none';
+        }
+};
+
+const renderNewMovieElement = (title,imageUrl,rating)=>{
+    const newMovieElement = document.createElement('li');
+    newMovieElement.className = 'movie-element';
+    newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+    <img src = "${imageUrl}"  alt = "${title}">
+    </div>
+    <div class="movie-element__info">
+    <h2>${title}<h2>
+    <p>${rating}/5 Stars</p>
+    </div>
+    `;
+    const listRoot = document.getElementById('movie-list');
+    listRoot.append(newMovieElement);
+};
 
 
 
