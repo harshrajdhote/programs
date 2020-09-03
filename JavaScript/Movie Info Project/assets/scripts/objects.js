20b17437f04e51ff35e355c68ac51b6a6a9608f7
harshrajdhote <dhoteharshraj@gmail.com>
2020-09-03 14:15:59 +0530

    Movie Info add and search button listeners done

--- JavaScript/Movie Info Project/assets/scripts/objects.js
+++ JavaScript/Movie Info Project/assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
   const movieList = document.getElementById('movie-list');
 
   if (movies.length === 0) {
@@ -14,12 +14,33 @@ const renderMovies = () => {
   }
   movieList.innerHTML = '';
 
-  movies.forEach((movie) => {
+
+const filteredMovies = !filter 
+? movies 
+: movies.filter((movie => movie.info.title.includes(filter)));
+
+
+
+  filteredMovies.forEach((movie) => {
     const movieEl = document.createElement('li');
-    movieEl.textContent = movie.info.title;
+    let text = movie.info.title + ' - '; 
+    for(const key in movie.info){
+      if(key !== 'title' ){
+        text = text + `${key}: ${movie.info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
     movieList.append(movieEl);
   });
 };
+
+
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+}
+
+
 
 const addMovieHandler = () => {
   const title = document.getElementById('title').value;
@@ -47,3 +68,4 @@ const addMovieHandler = () => {
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click',searchMovieHandler);
