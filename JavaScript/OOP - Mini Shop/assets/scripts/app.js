b04e01c9f5b805391854300d3cdcdb7f70bd4d85
harshrajdhote <dhoteharshraj@gmail.com>
2020-09-05 16:58:19 +0530

    web components added

--- JavaScript/OOP - Mini Shop/assets/scripts/app.js
+++ JavaScript/OOP - Mini Shop/assets/scripts/app.js
@@ -11,9 +11,43 @@ class Product {
     this.price = price;
   }
 }
-class ShoppingCart{
-  items = [];
 
+class ElementAttribute{
+  constructor(attrName,attrValue){
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component{
+  constructor(renderHookId){
+    this.hookId = renderHookId;
+  }
+
+  createRootElement(tag,cssClasses,attributes){
+    const rootElement = document.createElement(tag);
+    if(cssClasses){
+      rootElement.className = cssClasses;
+    }
+    if(attributes && attributes.length > 0)
+    {
+      for(const attr of attributes){
+        rootElement.setAttribute(attr.name,attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+
+
+  }
+}
+
+
+class ShoppingCart extends Component{
+  items = [];
+  constructor(renderHookId){
+    super(renderHookId);
+  }
   set cartItems(value){
     this.items = value;
     this.totalOutput.innerHTML = `<h2> Total: \$${this.totalAmount.toFixed(2)}</h2>`;
@@ -33,17 +67,18 @@ class ShoppingCart{
   }
 
   render(){
-    const cartEl = document.createElement('section');
+    
+    const cartEl = this.createRootElement('section','cart');
     cartEl.innerHTML = `
     <h2>Total: \$${0}</h2>
     <button>Order Now!</button>
     `
     cartEl.className = 'cart';
     this.totalOutput = cartEl.querySelector('h2');
-    return cartEl;
+    
   }
 }
-class ProductList{
+class ProductList extends Component{
   products = [
     new Product(
       'A Pillow',
@@ -59,9 +94,9 @@ class ProductList{
     )
   ];
   
-  constructor(product){
-    if(product) //no constructor overloading so here by default constuctor will be called with undefined
-     this.products.push(product);
+  constructor(renderHookId){
+  //  if(product) //no constructor overloading so here by default constuctor will be called with undefined
+    super(renderHookId);
   }
   
   addProduct(product){
@@ -70,23 +105,27 @@ class ProductList{
 
   render() {
     
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
+    this.createRootElement('ul','product-list', [
+      new ElementAttribute('id','prod-list')
+    ]);
+    //prodList.id = 'prod-list';
     // let i = 0;
     console.log(this.products);
     for (const prod of this.products) {
       console.log(prod);
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
-      prodList.append(prodEl);
+      const productItem = new ProductItem(prod,'prod-list');
+      productItem.render();
+     // prodList.append(prodEl);
      
     }
-   return prodList;
+  // return prodList;
   }
 };
 
-class ProductItem{
-  constructor(product){
+class ProductItem extends Component{
+  
+  constructor(product,renderHookId){
+    super(renderHookId);
     this.product = product;
   }
 
@@ -96,8 +135,7 @@ class ProductItem{
   }
 
   render(){
-    const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
+    const prodEl = this.createRootElement('li','product-item');
       prodEl.innerHTML = `
         <div>
           <img src="${this.product.imageUrl}" alt="${this.product.title}" >
@@ -111,19 +149,17 @@ class ProductItem{
       `;
       const addCartButton = prodEl.querySelector("button");
       addCartButton.addEventListener("click",this.addToCart.bind(this));
-      return prodEl;
+      
   }
 }
 
 class Shop{
   render(){
     const renderHook = document.getElementById('app');
-    this.cart = new ShoppingCart();
-    const cartEl = this.cart.render();
-    const productList = new ProductList();
-    const prodListEl = productList.render();
-    renderHook.append(cartEl);
-    renderHook.append(prodListEl);
+    this.cart = new ShoppingCart("app");
+    this.cart.render();
+    const productList = new ProductList("app");
+    productList.render();
   }
 }
 
