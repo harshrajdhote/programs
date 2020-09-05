1e37fa496a04cce83d7a8e15c1a2753d59122f54
harshrajdhote <dhoteharshraj@gmail.com>
2020-09-05 16:10:17 +0530

    class  shop Cart Value functionality added

--- JavaScript/OOP - Mini Shop/assets/scripts/app.js
+++ JavaScript/OOP - Mini Shop/assets/scripts/app.js
@@ -14,9 +14,21 @@ class Product {
 class ShoppingCart{
   items = [];
 
+  set cartItems(value){
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2> Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+  }
+
+  get totalAmount(){
+    const sum = this.items.reduce((prevValue,curItem)=>{
+      return prevValue + curItem.price;
+    },0);
+    return sum;
+  }
   addProduct(product){
-    this.items.push(product);
-    this.totalOutput.innerHTML = `<h2>Total : \$${1}</h2>`;
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
     //this.render();
   }
 
